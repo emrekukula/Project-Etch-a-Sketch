7577246dd4983aeab2dedb6cd78f5e13db385e30
emrekukula <emrekukula@gmail.com>
2023-08-30 22:58:53 +0300

    Added mouseover, selected events

--- scripts.js
+++ scripts.js
@@ -7,12 +7,23 @@ document.body.appendChild(grid);
 
 for (let i=0; i < column + 1 ; i++) {
     let column = document.createElement('div');
-    column.className = 'column';
+    column.className = `column${i}`;
     grid.appendChild(column);
     for (let j=0; j < row +1; j++) {
         let row = document.createElement('div');
-        row.className = 'row';
+        row.className = `row ${j}`;
         row.textContent = `${j} - ${i}`;
         column.appendChild(row);
     }
 }
+
+const rows = document.querySelectorAll('.row')
+rows.forEach ((element) => {
+    element.addEventListener('mouseover', () => {
+        element.classList.add('-selected')
+    })
+    element.addEventListener('click', () => {
+        element.classList.remove('-selected')
+    })
+}) 
+
