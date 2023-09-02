8c9a3b1d1576fbe8046bd1afaaffe22d8dcb8361
emrekukula <emrekukula@gmail.com>
2023-09-02 21:30:24 +0300

    Fixed overextending columns and added some conditions to user input as asked.

--- scripts.js
+++ scripts.js
@@ -12,7 +12,6 @@ for (let i=0; i < column + 1 ; i++) {
     for (let j=0; j < row +1; j++) {
         let row = document.createElement('div');
         row.className = `row ${j}`;
-        row.textContent = `${j} - ${i}`;
         column.appendChild(row);
     }
 }   
@@ -47,6 +46,8 @@ function customGrid() {
     grid.className = 'grid';
     document.body.appendChild(grid);
     userNumber = prompt('Grid Length?');
+    if (userNumber > 100) alert('Input can not exceed 100');
+    if (userNumber == isNaN) alert('Input must be a number');
     for (let i=0; i <= userNumber; i++) {
         let column = document.createElement('div');
         column.className = `column ${i}`;
@@ -54,11 +55,9 @@ function customGrid() {
         for (let j=0; j <= userNumber; j++) {
             let row = document.createElement('div');
             row.className = `row ${j}`;
-            row.textContent = `${j} - ${i}`;
             column.appendChild(row);
         }
     } 
     const rows = document.querySelectorAll('.row')
     coloring(rows);
 }
-
