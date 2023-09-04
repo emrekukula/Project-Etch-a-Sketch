0fceb997087dc811696e2f624e86043c3bd74ca7
emrekukula <emrekukula@gmail.com>
2023-09-04 21:42:19 +0300

    Added random coloring and brightness reduction onclick.

--- scripts.js
+++ scripts.js
@@ -5,28 +5,44 @@ const grid = document.createElement('div');
 grid.className = 'grid';
 document.body.appendChild(grid);
 
-for (let i=0; i < column ; i++) {
+for (let i=0; i < column; i++) {
     let column = document.createElement('div');
-    column.className = `column ${i}`;
+    column.className = 'column';
     grid.appendChild(column);
     for (let j=0; j < row; j++) {
         let row = document.createElement('div');
-        row.className = `row ${j}`;
+        row.className = 'row';
         column.appendChild(row);
     }
 }   
 
+
 const rows = document.querySelectorAll('.row')
 function coloring(line) {
     line.forEach ((element) => {
+        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
         element.addEventListener('mouseover', () => {
-            element.classList.add('-selected')
+            element.style.backgroundColor = randomColor
+            element.classList.add('-colored');
         })
         element.addEventListener('click', () => {
-            element.classList.remove('-selected')
+               if(element.className == 'row -colored') {
+                element.classList.add('B80');
+            } else if (element.classList == 'row -colored B80') {
+                element.classList.remove('B80')
+                element.classList.add('B60');
+            } else if (element.classList == 'row -colored B60') {
+                element.classList.remove('B60')
+                element.classList.add('B40');
+            } else if (element.classList == 'row -colored B40') {
+                element.classList.remove('B40')
+                element.classList.add('B20');
+            } else if (element.classList == 'row -colored B20') {
+                element.classList.remove('B20')
+                element.classList.add('B00');
+            }
         })
-    }) 
-} coloring(rows);
+    }) }  coloring(rows);
 
 
 function deleteBefore() {
@@ -46,15 +62,16 @@ function customGrid() {
     grid.className = 'grid';
     document.body.appendChild(grid);
     userNumber = prompt('Grid Length?');
+    
     if (userNumber > 100) alert('Can not exceed 100');
     if (isNaN(userNumber)) alert('Must be a number');
     for (let i=0; i <= userNumber - 1; i++) {
         let column = document.createElement('div');
-        column.className = `column ${i}`;
+        column.className = 'column';
         grid.appendChild(column);
         for (let j=0; j <= userNumber - 1; j++) {
             let row = document.createElement('div');
-            row.className = `row ${j}`;
+            row.className = 'row';
             column.appendChild(row);
         }
     } 
